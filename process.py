72f5558b2c1aa3511e42d8af86e86804b5135743
rb4279 <rb4279@naver.com>
2021-11-28 21:22:12 +0900

    text AC

--- process.py
+++ process.py
@@ -5,6 +5,10 @@ from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 import json
 import re
+import scipy.cluster.hierarchy as shc
+from sklearn.cluster import AgglomerativeClustering
+import numpy as np
+
 
 class Util:
     @staticmethod
@@ -30,10 +34,11 @@ class ContentProcessor:
         
         for data in json_data:
             sample_str = data['job'] + ' ' + data['name']
-            try:
-                sample_str += code_dict[data['code']]
-            except:
-                pass
+            # 이름과 직업명만 사용하고 contents 정보 일단 제외하기 위해서 
+            # try:
+            #     sample_str += code_dict[data['code']]
+            # except:
+            #     pass
             sample = kkma.nouns(sample_str)
             data['nouns'] = list(filter(Util.check_str_length, sample))
         
@@ -44,7 +49,7 @@ class SimilarityAnalyzer:
     @staticmethod
     def export_csv(list, name):
         tfidf_vectorizer = TfidfVectorizer(min_df=1,analyzer= 'word')
-        tfidf_matrix = tfidf_vectorizer.fit_transform(list)
+        tfidf_matrix = tfidf_vectorizer.fit_transform(list).toarray()                ## toarray 추가
 
         df1 = pd.DataFrame(tfidf_matrix)
         df1.to_csv('tfidf_job_' + name + '.csv')
@@ -74,15 +79,15 @@ class SimilarityAnalyzer:
 
 class Recommender:
     @staticmethod
-    def recommend(job_index):
+    def recommend_cossin(job_index):
         with open('job_detail_list.json', 'r',encoding='UTF-8') as f:
             json_data = json.load(f)
 
         cossim_detail_matrix= pd.read_csv('cossim_job_detail0.csv')
         cossim_address_matrix= pd.read_csv('cossim_job_address.csv')
         
-        sim_detail_scores = list(enumerate(cossim_detail_matrix.iloc[job_index]))
-        sim_address_scores = list(enumerate(cossim_address_matrix.iloc[job_index]))
+        sim_detail_scores = list(enumerate(cossim_detail_matrix.iloc[job_index][1:])) ## 인덱스영역 제거
+        sim_address_scores = list(enumerate(cossim_address_matrix.iloc[job_index][1:]))
 
         sum_scores = []
         for score in sim_detail_scores:
@@ -91,16 +96,34 @@ class Recommender:
             sum_scores.append(new_score)
 
         sim_scores = list(enumerate(sum_scores))
-        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)  
 
-        sim_scores = sim_scores[1:11]
+        sim_scores = sim_scores[0:11] ## 자신포함
 
         index_list = [i[0] for i in sim_scores]
         print(json_data[job_index])
-        print([json_data[i]['job'] for i in index_list])
+        # print([[json_data[i]['job']] for i in index_list])
+        for i in index_list :
+            print( json_data[i]['name'] + '  ,  ' + json_data[i]['job'] )
+    @staticmethod
+    def recommend_AC(job_index):
+        with open('job_detail_list.json', 'r',encoding='UTF-8') as f:
+            json_data = json.load(f)
+        
+        tfidf_vector = pd.read_csv('tfidf_job_detail0.csv')
+        vector = tfidf_vector.iloc[:, 1:].values
+
+        cluster = AgglomerativeClustering(n_clusters=100, affinity='euclidean', linkage='ward')
+        cluster_list = cluster.fit_predict(vector)
+
+        index_list = np.where(cluster_list == cluster_list[job_index])[0]  # 일단 자신포함
+        print(json_data[job_index])
+        # print([[json_data[i]['job']] for i in index_list])
+        for i in index_list :
+            print( json_data[i]['name'] + '  ,  ' + json_data[i]['job'] )
         
 
 if __name__ == "__main__":
-    ContentProcessor.split_content()
+    # ContentProcessor.split_content()
     # SimilarityAnalyzer.analyze()
-    # Recommender.recommend(2)
+    Recommender.recommend_AC(0)
